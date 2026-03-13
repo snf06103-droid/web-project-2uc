e97cf4c31823c1e19bb1806755844d0275867afb
장윤서 <snf06103@naver.com>
2026-03-13 12:51:20 +0900

    메인페이지 기능 넣는 중

--- js/main.js
+++ js/main.js
@@ -28,6 +28,17 @@ document.addEventListener("DOMContentLoaded",()=>{
             delay: 3000,
         },
         });
+
+
+        var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 5,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
 })
 
 
